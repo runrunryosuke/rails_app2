9482f8a9d45bd4e19bccc509858269b1b32ad830
runrunryosuke <runrunryosuke0425@gmail.com>
2021-03-16 07:35:44 +0900

    14 chapter 14.22

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,10 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :Relationships, only: [:create, :destroy]
 end
